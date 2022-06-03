49a298fec3c3ed638aff201d71691f0d0c1752d9
rirungayu <rirungayu@gmail.com>
2022-06-03 03:27:07 -0700

    intel fix commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, err);
+	fprintf(stderr, msg);
 	return (1);
 }
