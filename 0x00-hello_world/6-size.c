f82dd39c79a31a24ea8c3208a7c9534a2c213597
rirungayu <rirungayu@gmail.com>
2022-06-03 02:50:49 -0700

    6-size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	printf("Size of a char:%d\n", sizeof(char);
+	printf("Size of a char:%d\n", sizeof(char));
 	printf("Size of an int:%d\n", sizeof(int));
 	printf("Size of a long int:%d\n", sizeof(long int));
 	printf("Size of a long long int:%d\n", sizeof(long long int));
