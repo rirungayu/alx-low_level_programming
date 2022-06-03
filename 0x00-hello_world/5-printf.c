4a76b85d15c8a4adf932ac6c7578bc9e55ef0db0
rirungayu <rirungayu@gmail.com>
2022-06-03 02:11:40 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,10 +9,8 @@
 
 int main(void)
 {
-	char outstr1[] = "with proper grammar,";
-	char outstr2[] = " but the outcome is a piece of art,";
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
 
-	strcat(outstr1, outstr2);
 	printf(outstr1);
 	return (0);
 }
