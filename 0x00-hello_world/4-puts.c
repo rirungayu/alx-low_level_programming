77065b1646aba9021797e65660a0e2a0551d864e
rirungayu <rirungayu@gmail.com>
2022-06-03 01:31:17 -0700

    puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main(void){
 
-	char outstr[] ="\"Programming is like building a multilingual puzzle";
+/* c program to putput the string 
+ * "\"Programming is like building a multilingual puzzle into stdout" 
+ * using puts function */
+
+int main(void)
+{
+
+	char outstr[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(outstr);
-	return 0;
+	return (0);
 }
