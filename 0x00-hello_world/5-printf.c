63b7ad0d343ce47816ae9174b22266524a43e60f
rirungayu <rirungayu@gmail.com>
2022-06-03 02:02:16 -0700

    printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char outstr[] = "prints with proper grammar,";
+	char outstr2[] = "but the outcome is a piece of art,";
+
+	printf(outstr1, outstr2);
+	return (0);
+}
