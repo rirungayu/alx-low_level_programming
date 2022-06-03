4cd1cee8a7dc82a4f681a82ad92445e4ac5300e9
rirungayu <rirungayu@gmail.com>
2022-06-03 03:23:46 -0700

    quote fix commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, err);
 	return (1);
 }
