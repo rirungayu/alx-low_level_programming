cadc4eabfb9df51ace5f4eefd03afd3ca336a21b
rirungayu <rirungayu@gmail.com>
2022-06-03 02:49:01 -0700

    size commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of types on the computer it is compiled and run on
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	printf("Size of a char:%d\n", sizeof(char);
+	printf("Size of an int:%d\n", sizeof(int));
+	printf("Size of a long int:%d\n", sizeof(long int));
+	printf("Size of a long long int:%d\n", sizeof(long long int));
+	printf("Size of a float:%d\n", sizeof(float));
+	return (0);
+}
