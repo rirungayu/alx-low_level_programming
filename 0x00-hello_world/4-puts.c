21dcde361b5990c67ad70e40a325d1587383c03d
rirungayu <rirungayu@gmail.com>
2022-06-03 01:42:51 -0700

    code fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/* c program to putput the string 
- * "\"Programming is like building a multilingual puzzle into stdout" 
- * using puts function */
+/*
+ * c program to output the string
+ * "\"Programming is like building a multilingual puzzle into stdout"
+ * using puts function
+ */
 
 int main(void)
 {
-
 	char outstr[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(outstr);
 	return (0);
 }
