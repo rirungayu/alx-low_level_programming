e84459e63b7a31615f2f9b60203383ae44e1ab0e
rirungayu <rirungayu@gmail.com>
2022-06-07 12:15:06 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 	int numz[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(numz[i] + '0');
+		if (i == 9)
+		{
+			break;
+		}
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(numz[9] + '0');
-
 	return (0);
 }
