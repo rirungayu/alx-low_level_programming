0d55e359ea24c6571b6c80223b1646a8272665b9
rirungayu <rirungayu@gmail.com>
2022-06-07 01:30:00 -0700

    base 16 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all base 16 numbers
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+	char hex [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+	char hexx[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(hex[i]);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(hexx[i]);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
