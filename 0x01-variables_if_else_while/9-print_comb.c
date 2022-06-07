ced2507279792c645dc77ba7530e3070efccd9eb
rirungayu <rirungayu@gmail.com>
2022-06-07 12:00:34 -0700

    print comb commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of single digit numbers
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int numz[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(numz[i] + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
