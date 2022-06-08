36f59d573741e72b9f09208eda183e03993794c3
rirungayu <rirungayu@gmail.com>
2022-06-08 01:59:49 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints entire alphabet
+ *
+ * Return: zero
+ */
+
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+	_putchar(alphabet[i]);
+	i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,37 @@
-int _putchar(char);
+#ifndef MAIN_DOT_H
+#define MAIN_DOT_H
 
+/* function to print the alphabet */
+void print_alphabet(void);
+
+/* function to print the alphabet 10 times */
+void print_alphabet_x10(void);
+
+/* function that checks for lowercase character */
+int _islower(int c);
+
+/* function that checks for alphabetic character */
+int _isalpha(int c);
+
+/* function that prints the sign of a number */
+int print_sign(int n);
+
+/* function that computes the absolute value of an integer */
+int _abs(int);
+
+/* function that prints the last digit of a number */
+int print_last_digit(int);
+
+/* function that prints every minute of the day of Jack Bauer */
+void jack_bauer(void);
+
+/* function that prints the 9 times table, starting with 0 */
+void times_table(void);
+
+/* function that adds two integers and returns the result */
+int add(int, int);
+
+/* function that prints all natural numbers from n to 98 */
+void print_to_98(int n);
+
+#endif
