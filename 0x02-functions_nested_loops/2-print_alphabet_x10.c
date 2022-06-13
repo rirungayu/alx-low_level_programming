30fac06fa55927bab6b00827b50e899bc183fd7c
rirungayu <rirungayu@gmail.com>
2022-06-13 00:15:05 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ *
+ * Retirn : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int n, i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+		_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
