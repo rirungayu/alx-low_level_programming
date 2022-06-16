29da867f22d35b860cc06eacc6bc19779bae7c7c
rirungayu <rirungayu@gmail.com>
2022-06-16 14:32:05 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value
+ * @x: number to be changes
+ *
+ * Return: absolute value
+ */
+
+int _abs(int x)
+{
+int c;
+
+if (x >= 0)
+	c = x;
+else
+	c = x * -1;
+
+	return (c);
+}
