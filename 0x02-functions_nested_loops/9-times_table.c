9fe3474cc12eb850bc52fde434bb8121c5f82200
rirungayu <rirungayu@gmail.com>
2022-06-16 16:26:21 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times tables
+ *
+ * Return: nothing
+ */
+
+void times_table(void){
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+int f = j * i;
+if(f > 9)_putchar((f/10)+'0');
+_putchar((f%10)+'0');
+_putchar(',');
+if(f < 10 && j != 9)_putchar('.');
+if(j < 9)_putchar('.');
+if(j == 9)_putchar('$');
+_putchar('\n');
+}
+}
+}
