12f6434446098b583fcd640dcd556cce01b3faf6
rirungayu <rirungayu@gmail.com>
2022-06-16 14:58:19 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - prints last digit of a number
+ * @x: number to be checked
+ *
+ * Return: the digit
+ */
+
+int print_last_digit(int x)
+{
+	int dig = abs(x % 10);
+
+	return (dig);
+
+}
