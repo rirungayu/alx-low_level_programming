39559bc6416aff8c911b0fac975b36af42462c1e
rirungayu <rirungayu@gmail.com>
2022-06-16 17:26:20 -0700

    98

---
+++ 0x02-functions_nested_loops/11-pint_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints number until 98
+ * @n: number to check
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+int mx = 98;
+
+if (i < mx)
+{
+while (i < mx)
+{
+printf("%d", i);
+i ++;
+}
+}
+
+else if (i < mx)                                                                 {
+while (i < mx)
+{
+	i ++;
+}
+printf("%d", mx);
+}
+}
