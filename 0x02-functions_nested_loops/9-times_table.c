80464866fd423aee2735f61007b0a0efcb06a2ca
rirungayu <rirungayu@gmail.com>
2022-06-16 18:46:08 -0700

    final

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,15 +15,15 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 int f = j * i;
+if (f < 10 && j != 0)
+_putchar(' ');
+if (j != 0)
+_putchar(' ');
 if (f > 9)
 	_putchar((f / 10) + '0');
 _putchar((f % 10) + '0');
 if (j != 9)
 	_putchar(',');
-if (f < 10 && j != 9)
-	_putchar(' ');
-if (j < 9)
-	_putchar(' ');
 }
 _putchar('\n');
 }
