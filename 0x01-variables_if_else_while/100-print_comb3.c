416be7b9c4a61f707dcd4c011398284a1a6da25c
rirungayu <rirungayu@gmail.com>
2022-09-12 05:01:39 -0700

    comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all 2 digit combinations
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int number = 0;
+	int max = 100;
+
+	while (number < max)
+	{
+		int first = number / 10;
+		int second = number % 10;
+
+		number = number + 1;
+		if (first - second >= 0)
+		{
+			continue;
+		}
+		putchar(first % 10 + '0');
+		putchar(second % 10 + '0');
+
+		if (number == 90)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
