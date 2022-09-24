9e5a15d8ae4bd19494f14ac303d1347091e8e760
rirungayu <rirungayu@gmail.com>
2022-09-24 12:55:59 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a function to check for common multiples
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", n);
+	n++;
+	}
+
+	return (0);
+}
