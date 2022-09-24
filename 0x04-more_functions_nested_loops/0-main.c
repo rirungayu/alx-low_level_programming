5e178fca5108109a6682ab85da07a54838ba61f4
rirungayu <rirungayu@gmail.com>
2022-09-24 12:39:01 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -6,15 +6,12 @@
 
 int main(void)
 {
-	char s1[98] = "Hello";
-	char s2[] = "World\n";
-	char *ptr;
+	char c;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+
 	return (0);
 }
