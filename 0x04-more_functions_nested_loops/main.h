617991401725fade250f75592ba8668ff3d128a1
rirungayu <rirungayu@gmail.com>
2022-09-24 12:30:55 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,37 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/*checks if character is uppercase*/
+int _isupper(int c);
+
+/*checks if character is integer*/
+int _isdigit(int c);
+
+/*multiplies two integers*/
+int mul(int a, int b);
+
+/*prints 0 to 9*/
+void print_numbers(void);
+
+/*prints 0 to 9 except 2 and 4*/
+void print_most_numbers(void);
+
+/*prints 0 to 14 ten times*/
+void more_numbers(void);
+
+/*draws a straight line in the terminal*/
+void print_line(int n);
+
+/*draws diagonal line in the terminal*/
+void print_diagonal(int n);
+
+/*prints a square*/
+void print_square(int size);
+
+/*prints triangle*/
+void print_triangle(int size);
+
+/*prints and integer*/
+void print_number(int n);
+
+#endif
