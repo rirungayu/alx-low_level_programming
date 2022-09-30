0858b4a00728dfdf35d48a6cc7e2ab513f0a2d86
rirungayu <rirungayu@gmail.com>
2022-09-30 14:01:06 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - returns name of the executable
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
