e84f63cb13d75b45c2bcb51d2da7f2775393c50a
rirungayu <rirungayu@gmail.com>
2022-09-30 06:56:24 -0700

    library created

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int c);
+int _isupper(int c);
+int isdigit(int c);
+int strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src);
+char *_strncpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char *c);
+unsigned int _strsp(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_srstr(char *haystack, char *needle);
+
+#endif /*MAIN_H*/
