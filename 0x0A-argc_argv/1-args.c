f13a506a8a3b63f76964061de565899466ab4c19
rirungayu <rirungayu@gmail.com>
2022-09-30 14:16:39 -0700

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,14 @@
 /**
  * main - prints number of arguments passes
  * @argc: first argument
+ * @argv: array of arguments
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int arguments = argc - 1;
-	
+
 	argv[0] = argv[0];
 	printf("%d\n", arguments);
 	return (0);
