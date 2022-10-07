b317debeba2e1af8f4c44d9af0a4b2aa78197d2a
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-07 10:53:55 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,23 +2,38 @@
 #include "main.h"
 
 /**
- * print_number - prints any integer
- * Return: nothing
+ * main - prints largest prime factor
+ * Return: 0
  */
 
-void print_number(int n)
+int main(void)
 {
+	long int n;
+	long int max;
+	long int i;
 
+	n = 612852475143;
+	max = -1;
 
-	if (n < 0)
+	while (n % 2 == 0)
 	{
-		n *= -1;
-		_putchar('-');
+		max = 2;
+		n /= 2;
 	}
 
-	if (n /10)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		print_number(n / 10);
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	putchar(n % 10 + '0');
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
