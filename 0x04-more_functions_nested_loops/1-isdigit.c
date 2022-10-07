20b39d5b08c4bf5b44c097afa9faf06cc993e844
rirungayu <rirungayu@gmail.com>
2022-10-06 22:20:42 -0700

    adding mul file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * _isdigit - checks if input is a digit
  * @c: input to be tested
  * Return: if digit return 1, if not return 0
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul- returns product of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
