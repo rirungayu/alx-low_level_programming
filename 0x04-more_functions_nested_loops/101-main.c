9a38cab96b83d48fcac6f6b4ba7da0b70ee1c58e
rirungayu <rirungayu@gmail.com>
2022-10-07 00:51:34 -0700

    adding print numbers function

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - calls print number function
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_number(-1098);
+	_putchar('\n');
+
+	return (0);
+}
+
+
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,6 +3,7 @@
 
 /**
  * print_number - prints any integer
+ * @n: number to be printed
  * Return: nothing
  */
 
@@ -12,13 +13,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n *= -1;
+		n = -n;
 		_putchar('-');
 	}
 
-	if (n /10)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
