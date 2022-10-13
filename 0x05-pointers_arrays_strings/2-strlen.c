5834bf452c87204d335a75109cf311c29b4f554b
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-13 19:50:33 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,12 +9,12 @@
 
 int _strlen(char *s)
 {
-  int length = 0;
-  
-  while (s[length] != '\0')
-  {
-    length++;
-  }
-  
-  return length;
+int length = 0;
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+return (length);
 }
