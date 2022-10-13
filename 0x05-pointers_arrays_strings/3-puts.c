ed0b2a63eb2cabf8ee6348a224d35d94f8bbfd96
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-13 19:55:55 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints a astring
+* @str: the string
+* Return: nothing
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+
+while(str[i] != '\0'){
+_putchar(str[i]);
+i++;
+}
+
+_putchar ('\n'); 
+}
+  
