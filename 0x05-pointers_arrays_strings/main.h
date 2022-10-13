0ffced4c3183c5d256fd43a26a5c4d03e63fe0a9
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-13 19:26:34 +0300

    Create main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,37 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* reset value to 98 */
+void reset_to_98(int *n);
+
+/*swaps two integers */
+void swap_int(int *a, int *b);
+
+/* return the length of a string */
+int _strlen(char *s);
+
+/* prints a string an new line */
+void _puts(char *str);
+
+/* print string in reverse */
+void print_rev(char *s);
+
+/* reverses a string */
+void rev_string(char *s);
+
+/* prints every other character of a string */
+void puts2(char *str);
+
+/* print half a string */
+void puts_half(char *str);
+
+/* prints n elements of an array */
+void print_array(int *a, int n);
+
+/* copies a string */
+char *_strcpy(char *dest, char *src);
+
+/* converts a string to an integer */
+int _atoi(char *s);
+
+#endif
