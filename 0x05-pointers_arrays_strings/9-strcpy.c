9b3d39b0e16b6c238d47db7852648e3e140e5b50
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-14 08:02:13 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,21 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
+int len = 0;
 
-while (i < 500)
+if(src[0] == '\0'){
+dest[0] = '\0'; 
+return dest;
+}
+while(src[len] != '\0'){
+len++;
+}
+len += 1;
+
+while (i < len)
 {
 dest[i] = src[i];
 i++;
-if (src[i] == '\0')
-break;
 }
 
 return (dest);
