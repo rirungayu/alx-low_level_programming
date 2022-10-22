0b951ddda0bd749aae78ff98756c496622924826
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-22 20:45:13 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,11 +2,11 @@
 
 /**
 * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
-
+*
 * @n: number of bytes
 * @s: memory area
 * @b: constant
-
+*
 * Return: pointer to s
 */
 
