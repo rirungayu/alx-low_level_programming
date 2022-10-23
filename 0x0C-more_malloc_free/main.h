8cf4a33bda3960c69173effa807b45dedf8935f5
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-23 05:25:20 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
