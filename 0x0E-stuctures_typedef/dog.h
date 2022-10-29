23a1b7318755ba830ad9048d0c5dc7df005f210b
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-29 22:52:19 +0300

    Update dog.h

--- 0x0E-stuctures_typedef/dog.h
+++ 0x0E-stuctures_typedef/dog.h
@@ -9,13 +9,19 @@
 * Description: a structure defining a dog
 */
 
-
-
 struct dog
 {
 char *name;
 float age;
 char *owner; 
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+typedef struct dog dog_t;
 
 #endif
