925e4b288242138a07594d3cdcc4336b38609f5e
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-30 00:51:43 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,18 @@
 #include "dog.h"
 
+/**
+* new_dog - creates a dog object
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+* Return: void
+*/
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t new_dog;
+new_dog = malloc(sizeof(dog_t));
 
+if (new_dog == NULL)
+return (NULL);
 }
