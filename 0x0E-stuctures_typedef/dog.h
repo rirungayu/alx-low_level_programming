1eb3c28e9918735762b71f29977a08a25a2f93bd
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-29 22:23:14 +0300

    Update dog.h

--- 0x0E-stuctures_typedef/dog.h
+++ 0x0E-stuctures_typedef/dog.h
@@ -1,6 +1,15 @@
 #define HEADER_FILE
 #ifndef HEADER_FILE
 
+/**
+* struct dog - dog struct
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+
+
+
 struct dog
 {
 char * name;
