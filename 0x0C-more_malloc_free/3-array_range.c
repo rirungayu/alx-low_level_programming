173931a3c939fcaca6924f8422d70403602e6bc9
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-30 08:52:30 +0300

    Update 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* array_range - fills array from range
+* @min: start value
+* @max: end value
+*/
+
+int *array_range(int min, int max)
+{
+int *arr;
+int i = 0;
+
+if (min > max)
+return NULL;
+arr = malloc(sizeof(int) * (max - min +1));
+if (arr == NULL)
+return NULL;
+
+while (min != max)
+{
+arr[i] = min;
+min++;
+i++;
+}
+
+return (arr);
+}
