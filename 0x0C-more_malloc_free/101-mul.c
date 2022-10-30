d25e3edcb26226f8bee8667b212c68b7dbe11b71
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-30 09:36:12 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
 unsigned long num1;
 unsigned long num2;
 unsigned long result;
-int i = 1;
-int j = 0;
+int i =;
+int j =;
 
 if (argc != 3)
 {
@@ -27,7 +27,7 @@ exit(98);
 num1 = atol(argv[1]);
 num2 = atol(argv[2]);
 
-while (i < argc)
+/*while (i < argc)
 {
 while (argv[i][j] != '\0')
 {
@@ -40,7 +40,18 @@ j++;
 }
 i++;
 }
+*/
+  
+  for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
+	}
 result = num1 * num2;
   
 printf("%lu\n", result);
