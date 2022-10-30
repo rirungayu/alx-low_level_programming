8c0f036cd7f4d272faf5ba1ff56e0c378512923a
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-10-30 08:23:53 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,15 @@
 /**
 * malloc_checked - allocates memory
 * @b: size of memory
-* Return: void
+* Return: void pointer
 */
 
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
+	void *ptr;
 
-ptr = malloc(b);
-if (ptr == NULL)
-exit(98);
-
-return (ptr):
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
