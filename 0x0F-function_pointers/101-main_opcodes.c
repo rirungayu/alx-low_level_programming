0ab22f18616e75b7a5f2d5178989a488c25841eb
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-02 07:19:37 +0300

    Create 101-main_opcodes.c

---
+++ 0x0F-function_pointers/101-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+int bytes;
+int i = 0;
+int (*prog)(int, char **) = &main;
+
+if (argc != 2)
+{
+printf("Error"):
+exit (1);
+}
+
+  
+bytes = atoi(argv[1])
+
+if (bytes < 0)
+{
+printf("Error");
+exit (2);
+}
+
+while (i < bytes)
+{
+printf("%02x%c", (unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
+i++;
+}
+
+
+return (0);
+}
