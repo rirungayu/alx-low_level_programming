45d3f8d8413cb03e2ae0ce326a2d3839983591ee
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-02 06:50:51 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,2 +1,37 @@
 #include "function_pointers.h"
 
+/**
+* op_add - gets sum of two numbers
+* op_sub - gets difference of two numbers
+* op_mul - gets product of two numbers
+* op_div - gets dividend of two numbers
+* op_mod - gets remainderof dividion
+* @a:first number
+* @b: second number
+* Return:allreturn result
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
