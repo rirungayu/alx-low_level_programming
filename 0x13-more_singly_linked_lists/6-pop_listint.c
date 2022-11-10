2c2c61f5ab12a471fb44f541eb31a1f07cfe4eb3
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 16:17:00 +0300

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,14 +11,13 @@ int pop_listint(listint_t **head)
 listint_t *node;
 int n;
 
-if (head)
-{
+if (*head == NULL)
+return (0);
+
 node  = *head;
 n = node->n;
 *head = node->next;
 free(node);
-}
+
 return (n);
-else
-return (0);
 }
