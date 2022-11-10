f64c6ccec32281dd769e72facf7fb026941d1a57
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 14:23:29 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
 section .data
-message: db "Hello, Holberton",0x0a
-msglen equ $-msg
+	msg: db "Hello, Holberton", 0x0a
+	msglen equ $-msg
 
 section .text
-global main
+	global main
 
 main:
-mov eax, 1 ; syscall for write
-mov edi, 1
-mov rsi, message
-mov edx, msglen
-syscall
-mov eax, 60 ; 60 is exit
-xor edi, edi ; exit (0)
-syscall
+	; write HelloHolberton to screen
+	mov eax, 1 ; syscall for write
+	mov edi, 1
+	mov rsi, msg
+	mov edx, msglen
+	syscall
+	mov eax, 60 ; 60 is exit
+	xor edi, edi ; exit (0)
+	syscall
