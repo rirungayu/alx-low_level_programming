39ad3cfb4fa36b1cd26ca953f57ab17c29d2a5a3
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 14:46:09 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+* print_listint - prints all the elements of a list
+* @h: list
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+size_t nodes = 0;
+
+while (h)
+{
+
+}
+return (nodes);
+}
