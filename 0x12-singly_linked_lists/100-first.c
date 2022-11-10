251175586759a8f4c20f7b4b3672238175f4601e
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 14:07:35 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
 * Return: nothing
 */
 
-void execBefore (void) __attribute__ ((constructor))
+void __attribute__ ((constructor)) execBefore (void) 
 {
 printf("You're beat! and yet, you must allow,"
        "\nI bore my house upon my back!\n");
