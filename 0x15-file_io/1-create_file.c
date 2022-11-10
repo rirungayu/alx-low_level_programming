d9ef4ff7244562733540222ef007ce37edf77c8e
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 20:35:04 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 ssize_t real_letters;
 int fd;
 
-if (filename == NULL)
+if (filename == NULL || text_content == NULL)
 return (-1);
 
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
