f215dd91891d2fd1ccb5e70517ecc45bc16cc7e5
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 09:34:55 +0300

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+/**
+* add_node - adds a new node at the beginning
+* @head: head of the list
+* @str: to be duplicated
+* Return: address of new node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t new_node = malloc(sizeof(list_t));
+
+if (new_node == NULL)
+return (NULL);
+else
+{
+new_node->str = strdup(str);
+new_node->next = *head;
+new_node->len = strlen(str);
+}
+
+return (new_node);
+}
