5d275a33414b6a04c3552c65cc0d2ab335bebf9e
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 16:07:20 +0300

    Create 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* pop_listint - remove head
+* @head: first node
+* Return: value iof the node
+*/
+
+int pop_listint(listint_t **head)
+{
+listint_t *node = *head;
+int n;
+
+n = node->n;
+*head = node->next;
+free(node);
+
+return (n);
+}
