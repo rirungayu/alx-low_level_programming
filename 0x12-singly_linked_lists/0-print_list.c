8f3f5fedda19fb2e328639e80e3966ad0a19116f
rirungayu <38273760+rirungayu@users.noreply.github.com>
2022-11-10 09:06:12 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,2 +1,24 @@
-#include "list.h"
+#include "lists.h"
 
+/**
+* print_list - prints a list
+* @h: the list
+* Return: size of list
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t nodes = 0;
+  
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len h->str);
+h = h->next;
+nodes++;
+}
+
+return (nodes);
+}
